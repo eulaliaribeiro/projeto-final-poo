ee7c882973f2f1b0b57fd263542c646298891699
Eulália Ribeiro <eulaliaribeiroecr@gmail.com>
2022-10-27 19:24:35 -0300

    criação da classe Mídia

---
+++ src/Midia.java
@@ -0,0 +1,53 @@
+/*
+Criar classe Pai chamada Midia
+
+Atributos:
+- codigo : String
+- preco : Double
+- nome : String
+
+Métodos:
+- darPlay()
+- construtor(todos os argumentos)
+- get() e set()
+ */
+
+public class Midia {
+    private String codigo;
+    private double preco;
+    private String nome;
+
+    public Midia(String codigo, double preco, String nome) {
+        this.codigo = codigo;
+        this.preco = preco;
+        this.nome = nome;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void darPlay(){
+        System.out.println("A mídia começou a ser reproduzida!");
+    }
+}
